5e2475a392499bb8910da2362bcdbcdde078d540
david geismar <davidgeismar@hotmail.fr>
2019-10-12 18:10:29 +0100

    all tests are passing

--- src/components/__tests__/LoginForm.test.js
+++ src/components/__tests__/LoginForm.test.js
@@ -15,100 +15,78 @@ let store
 const setup = (initialState = {}) => {
   store = makeMockStore(initialState);
   // console.log(store.getState())
-  const wrapper = shallow(
-    <Provider store={store}>
-      <LoginForm/>
-    </Provider>).dive()
+  const wrapper = shallow(<LoginForm store={store}/>).dive()
   return wrapper
 }
 
-
+//
 // describe('render', () => {
-//   describe('it renders with no errors', () => {
+  describe('it renders with no errors', () => {
     let initialState
     beforeEach(() => {
       initialState = { error: false, loading: false, authentication: { username: "", password: ""}, backgroundImage: null}
     })
-    // test('renders two stylish inputs on IOS', () => {
-    //   wrapper = setup(initialState)
-    //   expect(wrapper.find(StylishInput).length).toEqual(2);
-    // })
-    //
-    // test('renders two text inputs on Android', () => {
-    //   Platform.OS = 'android';
-    //   wrapper = setup(initialState)
-    //   expect(wrapper.find(TextInput).length).toEqual(2);
-    // })
+    test('renders two stylish inputs on IOS', () => {
+      wrapper = setup(initialState)
+      expect(wrapper.find(StylishInput).length).toEqual(2);
+    })
 
-    test('it fetches and displays an ImageBackground', (done) => {
-      moxios.install()
-      moxios.stubRequest('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', {
-        status: 200,
-        response: getImageOftheDayMock
-      })
-
-
-      wrapper = setup(initialState).dive()
-
-      moxios.wait(()=>{
-        wrapper.update()
-        console.log(store.getState())
-        console.log(wrapper.find(ImageBackground).prop('source').uri)
-        expect(wrapper.find(ImageBackground).prop('source').uri).toBe('/th?id=OHR.RedRocksArches_EN-GB2044389872_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp')
-        done()
-        moxios.uninstall()
-      })
+    test('renders two text inputs on Android', () => {
+      Platform.OS = 'android';
+      wrapper = setup(initialState)
+      expect(wrapper.find(TextInput).length).toEqual(2);
+    })
+  })
+//     // TESTS STORE + COMPONENT WHICH IS NOT NECESSARY
+//     // test('it fetches and displays an ImageBackground', (done) => {
+//     //   moxios.install()
+//     //   moxios.stubRequest('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', {
+//     //     status: 200,
+//     //     response: getImageOftheDayMock
+//     //   })
+//     //
+//     //
+//     //   wrapper = setup(initialState).dive()
+//     //
+//     //   moxios.wait(()=>{
+//     //     wrapper.update()
+//     //     console.log(store.getState())
+//     //     console.log(wrapper.find(ImageBackground).prop('source').uri)
+//     //     expect(wrapper.find(ImageBackground).prop('source').uri).toBe('/th?id=OHR.RedRocksArches_EN-GB2044389872_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp')
+//     //     done()
+//     //     moxios.uninstall()
+//     //   })
+//     // })
+//
+  describe('redux props', () => {
+    let initialState
+    let props
+    beforeEach(() => {
+      initialState = { error: false, loading: false, authentication: { username: "", password: ""}, backgroundImage: null}
+      wrapper = setup(initialState)
+      props = wrapper.instance().props
+    })
+    test('has username, password, error, loading, backgroundImage as props', ()=>{
+      expect(Object.keys(props)).toEqual(expect.arrayContaining(['username', 'password', 'error', 'loading', 'backgroundImage']))
+    })
+    test('has loginUser, authUpdate, fetchImageOfTheDay action creators as functions prop', ()=>{
+      expect(props.authUpdate).toBeInstanceOf(Function);
+      expect(props.loginUser).toBeInstanceOf(Function);
+      expect(props.fetchImageOfTheDay).toBeInstanceOf(Function);
     })
   })
 
-  // describe('logins user', () => {
-  //   let wrapper;
-  //   let initialState
-  //   beforeEach(() => {
-  //     initialState = { error: false, loading: false, authentication: { username: "", password: ""}, backgroundImage: null}
-  //   })
-  //   test('it logins user on click on emoticon', () => {
-  //     wrapper = setup(initialState).dive()
-  //     const loginUser = jest.spyOn(wrapper.instance(), 'loginUser');
-  //     console.log('wrapper instance', wrapper.instance())
-  //     console.log('loginuser', loginUser)
-  //     wrapper.find(Stressed).parent().simulate('click')
-  //     console.log('Stressed parent', wrapper.find(Stressed).parent().debug())
-  //     wrapper.update()
-  //     expect(loginUser).toHaveBeenCalled();
-  //   })
-  // })
-
-  // describe('redux props', () => {
-  //   test('has username, password, error, loading, backgroundImage as props', ()=>{
-  //     wrapper = setup(initialState),
-  //     console.log(wrapper.instance().props)
-  //     const props = wrapper.instance().props
-  //     expect(Object.keys(props)).toEqual(expect.arrayContaining(['username', 'password', 'error', 'loading', 'backgroundImage']))
-  //   })
-  //   test('has loginUser, authUpdate, fetchImageOfTheDay action creators as functions prop', ()=>{
-  //     wrapper = setup(initialState),
-  //     console.log(wrapper.instance().props)
-  //     const props = wrapper.instance().props
-  //     expect(props.authUpdate).toBeInstanceOf(Function);
-  //     expect(props.loginUser).toBeInstanceOf(Function);
-  //     expect(props.fetchImageOfTheDay).toBeInstanceOf(Function);
-  //   })
-  // })
-  // describe('it renders with an error mess', () => {
-  //     let wrapper;
-  //     beforeEach(() => {
-  //       const initialState = { error: 'je suis une erreur', loading: false, authentication: { username: "", password: ""}}
-  //       wrapper = setup(initialState)
-  //     })
-  //     test('displays an error mess', () => {
-  //       expect(wrapper.contains('je suis une erreur')).toBeTruthy()
-  //     })
-  //
-  //   })
-
-// })
-// 
+  describe('it renders with an error mess', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { error: 'je suis une erreur', loading: false, authentication: { username: "", password: ""}}
+      wrapper = setup(initialState)
+    })
+    test('displays an error mess', () => {
+      expect(wrapper.contains('je suis une erreur')).toBeTruthy()
+    })
+  })
+// //
 // test('fetchImageOfTheDay runs on LoginForm mount', () => {
 //   const fetchImageOfTheDayMock = jest.fn();
 //   const wrapper = shallow(<UnconnectedLoginForm fetchImageOfTheDay={fetchImageOfTheDayMock}/>)
@@ -117,3 +95,13 @@ const setup = (initialState = {}) => {
 //   const getFetchImageOfTheDayCount = fetchImageOfTheDayMock.mock.calls.length;
 //   expect(getFetchImageOfTheDayCount).toBe(1);
 // })
+
+describe('logins user', () => {
+  test('it logins user on click on emoticon', () => {
+    const loginUserMock = jest.fn();
+    const wrapper = shallow(<UnconnectedLoginForm loginUser={loginUserMock}/>)
+    wrapper.find(Stressed).parent().simulate('press')
+    const getloginUserCount = loginUserMock.mock.calls.length;
+    expect(getloginUserCount).toBe(1);
+  })
+})
--- src/reducers/__tests__/ActionKindsReducer.test.js
+++ src/reducers/__tests__/ActionKindsReducer.test.js
@@ -7,6 +7,6 @@ it("handles actions of type SET_ACTION_KINDS", () =>{
     type: SET_ACTION_KINDS,
     payload: { action_kinds: ['telephone', 'reunion'] }
   }
-    const newState = ClientReducer(INITIAL_STATE, action);
+    const newState = ActionKindsReducer(INITIAL_STATE, action);
     expect(newState).toEqual(['telephone', 'reunion'])
 })
