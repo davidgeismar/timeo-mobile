a8820db30e50c40517457aef4b24a352218e205e
david geismar <davidgeismar@hotmail.fr>
2019-10-15 11:13:16 +0100

    cleanup CardBlock

--- src/components/__tests__/CardBlock.test.js
+++ src/components/__tests__/CardBlock.test.js
@@ -5,9 +5,9 @@ import Card from '../Card'
 import { makeMockStore } from '../../../mocks/mockStore'
 
 
-let store
+
 const setup = (initialState = {}, cards) => {
-  store = makeMockStore(initialState);
+  const store = makeMockStore(initialState);
   const wrapper = shallow(<CardBlock cards={cards} store={store}/>).dive()
   return wrapper
 }
@@ -33,76 +33,55 @@ const cards = [{
     creationDate: '13/12/2019'
   }
 ]
-  describe('it renders with no errors', () => {
-    let initialState
-    beforeEach(() => {
-      initialState = {
-        user: {
-          user_info: {
-            logo_thumb: '/logo'
-          }
-        },
-        resources: {
-          resources: [
-            {
-              id: 'kiddk'
-            },
-            {
-              id: 'wieueie'
-            }
-          ]
-        }
+
+const initialState = {
+  user: {
+    user_info: {
+      logo_thumb: '/logo'
+    }
+  },
+  resources: {
+    resources: [
+      {
+        id: 'kiddk'
+      },
+      {
+        id: 'wieueie'
       }
-    })
-
-    test('renders cards', () => {
-      wrapper = setup(initialState, cards)
-      expect(wrapper.find(Card).length).toEqual(2);
-    })
+    ]
+  }
+}
+describe('it renders with no errors', () => {
+  test('renders cards', () => {
+    wrapper = setup(initialState, cards)
+    expect(wrapper.find(Card).length).toEqual(2);
   })
+})
 
-  describe('redux props', () => {
-    let initialState
-    let props
-    beforeEach(() => {
-      initialState = {
-        user: {
-          user_info: {
-            logo_thumb: '/logo'
-          }
-        },
-        resources: {
-          resources: [
-            {
-              id: 'kiddk'
-            },
-            {
-              id: 'wieueie'
-            }
-          ]
-        }
-      }
-      wrapper = setup(initialState, cards)
-      props = wrapper.instance().props
-    })
-    test('has logo_thumb, resources as props', ()=>{
-      expect(Object.keys(props)).toEqual(expect.arrayContaining(['resources', 'logo_thumb']))
-    })
-    test('has  setCurrentCard action creators as functions prop', ()=>{
-      expect(props.setCurrentCard).toBeInstanceOf(Function)
-    })
+describe('redux props', () => {
+  let props
+  beforeEach(() => {
+    wrapper = setup(initialState, cards)
+    props = wrapper.instance().props
   })
+  test('has logo_thumb, resources as props', ()=>{
+    expect(Object.keys(props)).toEqual(expect.arrayContaining(['resources', 'logo_thumb']))
+  })
+  test('has  setCurrentCard action creators as functions prop', ()=>{
+    expect(props.setCurrentCard).toBeInstanceOf(Function)
+  })
+})
 
-  describe('sets a currentCard', () => {
-    test('it sets a currentCard on press on card', () => {
-      const setCurrentCardMock = jest.fn();
-      const wrapper = shallow(
-        <UnconnectedCardBlock
-        setCurrentCard={setCurrentCardMock}
-        cards={cards}
-        />)
-      wrapper.find(Card).first().simulate('press')
-      const getsetCurrentCardCount = setCurrentCardMock.mock.calls.length;
-      expect(getsetCurrentCardCount).toBe(1);
-    })
+describe('sets a currentCard', () => {
+  test('it sets a currentCard on press on card', () => {
+    const setCurrentCardMock = jest.fn();
+    const wrapper = shallow(
+      <UnconnectedCardBlock
+      setCurrentCard={setCurrentCardMock}
+      cards={cards}
+      />)
+    wrapper.find(Card).first().simulate('press')
+    const getsetCurrentCardCount = setCurrentCardMock.mock.calls.length;
+    expect(getsetCurrentCardCount).toBe(1);
   })
+})
