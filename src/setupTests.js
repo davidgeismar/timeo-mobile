2e663528be82c1765ec0c0af02e99345ec15d8fb
david geismar <davidgeismar@hotmail.fr>
2019-10-15 12:15:54 +0100

    removing console

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-console.log('running setuptests')
 Enzyme.configure({
   adapter: new Adapter(),
   disableLifecycleMethods: true,
